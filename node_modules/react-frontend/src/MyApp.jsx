2795e0871c1ff7529e6f92ceba3dc505a5cadddb
ccuellar418 <107222183+ccuellar418@users.noreply.github.com>
2025-04-09 10:54:22 -0700

    Input for Name and Job

--- node_modules/react-frontend/src/MyApp.jsx
+++ node_modules/react-frontend/src/MyApp.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 // const characters = [
 //     {
@@ -21,24 +22,7 @@ import Table from "./Table";
 // ];
 
 function MyApp() {
-    const [characters, setCharacters] = useState([
-        {
-            name: "Charlie",
-            job: "Janitor"
-        },
-        {
-            name: "Mac",
-            job: "Bouncer"
-        },
-        {
-            name: "Dee",
-            job: "Aspring actress"
-        },
-        {
-            name: "Dennis",
-            job: "Bartender"
-        }
-    ]);
+    const [characters, setCharacters] = useState([]);
 
     function removeOneCharacter(index) {
         const updated = characters.filter((character, i) => {
@@ -49,12 +33,12 @@ function MyApp() {
 
     return (
         <div className="container">
-            <Table 
-                characterData={characters} 
-                removeCharacter={removeOneCharacter}
-            /> 
-             {/* that call to table basically just calls the function and lets table do its own thing */} 
+          <Table
+            characterData={characters}
+            removeCharacter={removeOneCharacter}
+          />
+          <Form />
         </div>
-    );
+      );
 }
 export default MyApp;
